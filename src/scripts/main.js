945aea33d75aa67c4aff67df5416794d8e99f496
Jan Kohlbach <mail@jankohlbach.dev>
2019-08-16 13:21:29 +0200

    version 5

---
+++ src/scripts/main.js
@@ -0,0 +1,37 @@
+const todoList = {
+  todos: [],
+  displayTodos() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+
+      for (let i = 0; i < this.todos.length; i += 1) {
+        if (this.todos[i].completed === true) {
+          console.log(`(x) ${this.todos[i].todoText}`);
+        } else {
+          console.log(`( ) ${this.todos[i].todoText}`);
+        }
+      }
+    }
+  },
+  addTodo(todoText) {
+    this.todos.push({
+      todoText,
+      completed: false,
+    });
+    this.displayTodos();
+  },
+  changeTodo(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted(position) {
+    this.todos[position].completed = !this.todos[position].completed;
+    this.displayTodos();
+  },
+};
